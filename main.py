44b41391b1896c272038e25ee6773cd542dc09cb
Jordan Williams <jordan@jwillikers.com>
2022-05-02 08:34:39 -0500

    Clean up code

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from machine import Pin, PWM
 
-# For PWM Use GPIO pin #29, which is pin A0 on the QT Py RP2040.
-pwm0 = PWM(Pin(29))
+MAX_DUTY_CYCLE = 65_535
+FORTY_PERCENT_DUTY_CYCLE = (MAX_DUTY_CYCLE * 2) // 5
 
 # PWM fans use a frequency of 25 kHz.
-pwm0.freq(25000)
+PWM_FAN_FREQUENCY = 25_000
+
+# For PWM use GPIO pin #29, which is pin A0 on the Adafruit QT Py RP2040.
+QT_PY_RP2040_PIN_A0 = 29
+
+pwm0 = PWM(Pin(QT_PY_RP2040_PIN_A0))
+
+pwm0.freq(PWM_FAN_FREQUENCY)
 
 # Reduce the speed of the fan to 40% by setting the PWM duty cycle to 40%.
-# The duty cycle can be set to a number between 0 and 65,535.
-# Thus, the required value is 65535 * 0.40 = 26,214.
-pwm0.duty_u16(26214)
+pwm0.duty_u16(FORTY_PERCENT_DUTY_CYCLE)
